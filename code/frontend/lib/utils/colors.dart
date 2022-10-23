4fb564938a9ac863da6e0236357bd2c8482659c7
Aniket Mishra <ianiket23@gmail.com>
2022-10-23 15:33:21 -0600

    1st page

---
+++ code/frontend/lib/utils/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const mobileBackgroundColor = Color.fromARGB(255,19, 21, 21);
+const webBackgroundColor = Color.fromRGBO(255,19, 21, 21);
+const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
+const buttonColor = Color.fromARGB(255, 206, 108, 71);
+const primaryColor = Colors.white;
+const secondaryColor = Colors.grey;
